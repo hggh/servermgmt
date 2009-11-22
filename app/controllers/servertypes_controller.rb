c0ef0adeee7a1e770845515c496b3062888ef85a
Jonas Genannt <jonas@valentina.brachium-system.net>
2009-11-22 17:49:42 +0100

    changed typo

--- app/controllers/servertypes_controller.rb
+++ app/controllers/servertypes_controller.rb
@@ -1,84 +1,84 @@
-class ServertypsController < ApplicationController
-  # GET /servertyps
-  # GET /servertyps.xml
+class ServertypeesController < ApplicationController
+  # GET /servertypes
+  # GET /servertypes.xml
   def index
-    @servertyps = Servertyp.find(:all)
+    @servertypes = Servertypee.find(:all)
 
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @servertyps }
+      format.xml  { render :xml => @servertypees }
     end
   end
 
-  # GET /servertyps/1
-  # GET /servertyps/1.xml
+  # GET /servertypes/1
+  # GET /servertypes/1.xml
   def show
-    @servertyp = Servertyp.find(params[:id])
+    @servertype = Servertype.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
-      format.xml  { render :xml => @servertyp }
+      format.xml  { render :xml => @servertype }
     end
   end
 
-  # GET /servertyps/new
-  # GET /servertyps/new.xml
+  # GET /servertypes/new
+  # GET /servertypes/new.xml
   def new
-    @servertyp = Servertyp.new
+    @servertype = Servertype.new
 
     respond_to do |format|
       format.html # new.html.erb
-      format.xml  { render :xml => @servertyp }
+      format.xml  { render :xml => @servertype }
     end
   end
 
-  # GET /servertyps/1/edit
+  # GET /servertypes/1/edit
   def edit
-    @servertyp = Servertyp.find(params[:id])
+    @servertype = Servertype.find(params[:id])
   end
 
-  # POST /servertyps
-  # POST /servertyps.xml
+  # POST /servertypes
+  # POST /servertypes.xml
   def create
-    @servertyp = Servertyp.new(params[:servertyp])
+    @servertype = Servertype.new(params[:servertype])
 
     respond_to do |format|
-      if @servertyp.save
-        flash[:notice] = 'Servertyp was successfully created.'
-        format.html { redirect_to(@servertyp) }
-        format.xml  { render :xml => @servertyp, :status => :created, :location => @servertyp }
+      if @servertype.save
+        flash[:notice] = 'Servertype was successfully created.'
+        format.html { redirect_to(@servertype) }
+        format.xml  { render :xml => @servertype, :status => :created, :location => @servertype }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @servertyp.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @servertype.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /servertyps/1
-  # PUT /servertyps/1.xml
+  # PUT /servertypes/1
+  # PUT /servertypes/1.xml
   def update
-    @servertyp = Servertyp.find(params[:id])
+    @servertype = Servertype.find(params[:id])
 
     respond_to do |format|
-      if @servertyp.update_attributes(params[:servertyp])
-        flash[:notice] = 'Servertyp was successfully updated.'
-        format.html { redirect_to(@servertyp) }
+      if @servertype.update_attributes(params[:servertype])
+        flash[:notice] = 'Servertype was successfully updated.'
+        format.html { redirect_to(@servertype) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @servertyp.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => @servertype.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /servertyps/1
-  # DELETE /servertyps/1.xml
+  # DELETE /servertypes/1
+  # DELETE /servertypes/1.xml
   def destroy
-    @servertyp = Servertyp.find(params[:id])
-    @servertyp.destroy
+    @servertype = Servertype.find(params[:id])
+    @servertype.destroy
 
     respond_to do |format|
-      format.html { redirect_to(servertyps_url) }
+      format.html { redirect_to(servertypes_url) }
       format.xml  { head :ok }
     end
   end
