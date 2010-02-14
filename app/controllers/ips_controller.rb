5ef3b3e86c56aa2dbc5cf009d096a8e5e6188866
Jonas Genannt <jonas@brachium-system.net>
2010-02-14 22:06:48 +0100

    added delete from network views

--- app/controllers/ips_controller.rb
+++ app/controllers/ips_controller.rb
@@ -2,12 +2,17 @@ class IpsController < ApplicationController
   
   def destroy
     @ip = Ip.find(params[:id])
-    server_id = @ip.server_id
+    server_id  = @ip.server_id
+    network_id = @ip.network_id
     @ip.destroy
     
     respond_to do |format|
       flash[:notice] = 'IP address was successfully deleted.'
-      format.html { redirect_to(:controller => 'servers', :action => "show", :id=> server_id ) }
+      if params[:from] and params[:from] == "network_view"
+        format.html { redirect_to(:controller => 'networks', :action => "show", :id => network_id) } 
+      else
+        format.html { redirect_to(:controller => 'servers', :action => "show", :id=> server_id ) }
+      end
     end
   end
   
