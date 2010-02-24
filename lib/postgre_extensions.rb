aa8948ba992bebd43d64f2c4210ca63246bef5ba
Jonas Genannt <jonas@brachium-system.net>
2010-02-25 00:45:36 +0100

    added support on rails for postgre 'inet' datatype

---
+++ lib/postgre_extensions.rb
@@ -0,0 +1,76 @@
+class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter < ActiveRecord::ConnectionAdapters::AbstractAdapter
+  NATIVE_DATABASE_TYPES = {
+        :primary_key => "serial primary key".freeze,
+        :string      => { :name => "character varying", :limit => 255 },
+        :text        => { :name => "text" },
+        :integer     => { :name => "integer" },
+        :float       => { :name => "float" },
+        :decimal     => { :name => "decimal" },
+        :datetime    => { :name => "timestamp" },
+        :timestamp   => { :name => "timestamp" },
+        :time        => { :name => "time" },
+        :date        => { :name => "date" },
+        :binary      => { :name => "bytea" },
+        :boolean     => { :name => "boolean" },
+        :xml         => { :name => "xml" },
+        :inet        => { :name => "inet" }
+  }
+end
+
+class ActiveRecord::ConnectionAdapters::TableDefinition
+  def inet(*args)
+    options = args.extract_options!
+    column(args[0], 'inet', options)
+  end
+end
+
+class ActiveRecord::ConnectionAdapters::PostgreSQLColumn
+  private
+  def simplified_type(field_type)
+    case field_type
+      # Numeric and monetary types
+      when /^(?:real|double precision)$/
+              :float
+      # Monetary types
+      when /^money$/
+              :decimal
+      # Character types
+      when /^(?:character varying|bpchar)(?:\(\d+\))?$/
+              :string
+      # Binary data types
+      when /^bytea$/
+              :binary
+      # Date/time types
+      when /^timestamp with(?:out)? time zone$/
+              :datetime
+      when /^interval$/
+              :string
+      # Geometric types
+      when /^(?:point|line|lseg|box|"?path"?|polygon|circle)$/
+              :string
+      # Hack: We want to use inet type
+      when /^inet$/
+              :inet
+      # Network address types
+      when /^(?:cidr|inet|macaddr)$/
+              :string
+      # Bit strings
+      when /^bit(?: varying)?(?:\(\d+\))?$/
+              :string
+      # XML type
+      when /^xml$/
+              :xml
+      # Arrays
+      when /^\D+\[\]$/
+              :string
+      # Object identifier types
+      when /^oid$/
+              :integer
+      # Pass through all types that are not specific to PostgreSQL.
+    else
+      super
+    end
+  end
+  
+end
+
