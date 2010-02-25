4d84993d4076d4cf5884bf567862371a0474dcb8
Jonas Genannt <jonas@brachium-system.net>
2010-02-25 22:50:56 +0100

    added gateway for networks

--- app/models/ip.rb
+++ app/models/ip.rb
@@ -1,18 +1,5 @@
 class Ip < ActiveRecord::Base
   default_scope :order => :ip
-  require 'ipaddr'
-  def is_ipaddress
-    begin
-      net = IPAddr.new ip
-      if net.ipv4? or net.ipv6?
-        return true
-      else
-      end
-    rescue 
-      errors.add(:ip, " is not valid")
-      return false
-    end
-  end
   
   belongs_to :network
   belongs_to :server
@@ -21,10 +8,19 @@ class Ip < ActiveRecord::Base
   validates_presence_of :ip
   validates_presence_of :network_id
   validates_presence_of :ip_type
-  validate :is_ipaddress
-  validates_uniqueness_of :ip, :if => :is_ipaddress
-  validates_uniqueness_of :is_primary, :if => :is_ipaddress, :scope => [ :ip, :server_id ]
-  
-  
-  
+  validate :checkip
+  validates_uniqueness_of :ip, :if => :checkip
+  validates_uniqueness_of :is_primary, :if => :checkip, :scope => [ :ip, :server_id ]
+
+  def checkip
+    if ServerManager.new.is_ipaddress(ip)
+      return true
+    else
+      if errors.count < 1
+        errors.add('IP Address')
+      end
+      return false
+    end
+  end
+
 end
--- app/models/network.rb
+++ app/models/network.rb
@@ -1,13 +1,13 @@
 class Network < ActiveRecord::Base
   default_scope :order => :network
   
-	validates_format_of :network, :with => /^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\/[0-9]{1,2}$/, :on => :save
+	validate :check_network
 	validates_presence_of :comment
 	validates_length_of :comment, :minimum => 4
   validates_presence_of :customer
   
-  validates_uniqueness_of :network, :scope => :customer_id
-  
+  validates_uniqueness_of :network, :scope => :customer_id, :if => :check_network
+  #FIXME: We should check if Gateway is in the network range!
   
   belongs_to :customer
 	has_many :ips, :dependent => :destroy
@@ -15,4 +15,29 @@ class Network < ActiveRecord::Base
   def self.NetworkbyCust(customerid)
     Network.find(:all, :conditions => "customer_id = #{customerid}")
   end
+
+
+  def gw=(gateway)
+    gw_clean = ''
+    if ServerManager.new.is_ipaddress(gateway)
+      gw_clean = gateway
+    else
+      if check_network
+        ipaddr = IPAddr.new(network.split('/')[0])
+        gw_clean = IPAddr.new(ipaddr.to_i + 1 , Socket::AF_INET).to_s
+      end
+    end
+    write_attribute(:gw, gw_clean)
+  end
+
+  def check_network
+    if ServerManager.new.is_network(network)
+      return true
+    end
+    if errors.count < 1
+      errors.add('Network')
+    end
+    false
+  end
+
 end
