47ef7f7c2626454307880ed46148b100dbfd0c4a
Jonas Genannt <jonas@brachium-system.net>
2010-03-04 19:19:09 +0100

    added helper function for redirect after destroy

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def slink_to(key_name, key_value, name, options = nil, html_options = nil)
+    key_name  = "default" if key_name == nil
+    key_value = controller_name() if key_value == nil
+    session[key_name] = key_value
+    link_to(name, options, html_options)
+  end
 end
