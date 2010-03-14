27f146d4dd03aa5fed6ca43f40eadc4af5629995
Jonas Genannt <jonas@brachium-system.net>
2010-03-14 17:16:50 +0100

    added new checks for domain_records; added edit domain site

--- app/models/domain_record.rb
+++ app/models/domain_record.rb
@@ -1,8 +1,8 @@
 class DomainRecord < ActiveRecord::Base
   validates_presence_of :ttl
-  validates_presence_of :value
   validates_presence_of :domain_record_type_id
   validates_presence_of :domain_id
+  validate :check_value_record
   validates_numericality_of :ttl
 
 	belongs_to :domain
@@ -14,22 +14,53 @@ class DomainRecord < ActiveRecord::Base
   def value=(content)
     clean = content
     # RecordType is PTR and no dot on end, we will add one
-    if domain_record_type_id == 11 and content !~ /\.$/
+    if domain_record_type.name == 'PTR' and content !~ /\.$/ and content.length > 0
       clean = content + '.'
     end
     # RecordType is NS and no dot on end, we will add one
-    if domain_record_type_id == 9 and content !~ /\.$/
+    if domain_record_type.name == 'NS' and content !~ /\.$/
       clean = content + '.'
     end
     # RecordType is MX and no dot on end, we will add one
-    if domain_record_type_id == 7 and content !~ /\.$/
+    if domain_record_type.name == 'MX' and content !~ /\.$/
       clean = content + '.'
     end
     write_attribute(:value, clean)
   end
   private
-    def update_serial
-      domain = Domain.find(domain_id)
-      domain.touch
+  def update_serial
+    domain = Domain.find(domain_id)
+    domain.touch
+  end
+
+  def check_value_record
+    if value.length == 0
+      errors.add('content', 'Could not be empty.')
+      return false
     end
+    if domain_record_type.name == 'PTR'
+      if source !~ /^[0-9]+$/ or !source
+        errors.add('Type', ' is PTR, Name sould be an integer.')
+      end
+      if value.length == 0 or value !~ /^[A-Za-z0-9\-_\.]+$/
+        errors.add('content', 'bad charactor for content')
+      end
+    end
+    if domain_record_type.name == 'A'
+      if ServerManager.new.is_ipv4?(value) == false
+        errors.add('content', 'is not an valid IPv4 address')
+      end
+    end
+    if domain_record_type.name == 'AAAA'
+      if ServerManager.new.is_ipv6?(value) == false
+        errors.add('content', 'is not an valid IPv6 address')
+      end
+    end
+    if domain_record_type.name == 'MX'
+      if extra_value !~ /^[0-9]+$/ or !extra_value
+        errors.add('MX', ' records needs an Prio.')
+      end
+    end
+  end
+
 end
--- lib/server_manager.rb
+++ lib/server_manager.rb
@@ -12,7 +12,31 @@ class ServerManager
     end
     return false
   end
-  
+
+  def is_ipv4? ip
+    begin
+      net = IPAddr.new ip
+      if net.ipv4?
+        return true
+      end
+    rescue
+      false
+    end
+    false
+  end
+
+  def is_ipv6? ip
+    begin
+      net = IPAddr.new ip
+      if net.ipv6?
+        return true
+      end
+    rescue
+      false
+    end
+    false
+  end
+
   def is_network network
     begin
       net = IPAddr.new network
