6f3efa00110b4c20d58ce0a0c67271892dd8cc71
Jonas Genannt <jonas@brachium-system.net>
2011-04-24 15:20:18 +0200

    removed Domains Observer and forking of job. does not work

--- config/application.rb
+++ config/application.rb
@@ -66,7 +66,7 @@ module Servermgmt
   
     # Activate observers that should always be running
     # config.active_record.observers = :cacher, :garbage_collector
-    config.active_record.observers = :domain_observer
+    #config.active_record.observers = :domain_observer
 
   end
 end
