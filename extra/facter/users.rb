f8d79cdb854d7ff89376d8eecec06c71e3024c9d
Jonas Genannt <jonas@brachium-system.net>
2011-05-20 21:08:09 +0200

    fixed typo with user id  1000

--- extra/facter/users.rb
+++ extra/facter/users.rb
@@ -7,7 +7,7 @@ Facter.add("users") do
         getent_output.split("\n").each do |line|
           next if line =~ /^nobody/
 
-          users << line.split(":")[0] if line.split(":")[2].to_i > 1000 or line.split(":")[2].to_i == 0
+          users << line.split(":")[0] if line.split(":")[2].to_i >= 1000 or line.split(":")[2].to_i == 0
         end
 
         users.join(',')
