0a7535355e41ca88020b0b035e26661537c92796
Adrian Rangel <adrian.rangel@gmail.com>
2013-12-21 12:19:36 -0600

    secure secret_token

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,1 +1,5 @@
-Servermgmt::Application.config.secret_token = "dsfjkldasfasfdjkl435jo543901790fjdsklafjdosfu9032913odagdsggdffd"
+Servermgmt::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+  ('x' * 30) # meets minimum requirement of 30 chars long
+else
+  ENV['SECRET_TOKEN']
+end
